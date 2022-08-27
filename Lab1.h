e3de3d119d28f8ec8e5654d94ea054fb2e722b0b
snuffles5 <daniel.eni@gmail.com>
2022-08-27 09:44:56 +0300

    syncing

--- Lab1.h
+++
@@ -1,3 +0,0 @@
-//
-// Created by Daniel Eni Kandil on 08/07/2022.
-//
--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "Lab1.cpp"
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    Lab1();
-    return 0;
-}
