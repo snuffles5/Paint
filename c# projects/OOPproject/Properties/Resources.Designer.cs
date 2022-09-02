e68e6d0dbd010d7df9571507e7d6eb0ca225e247
Yatal42 <taltal2345@gmail.com>
2022-09-02 13:26:48 +0300

    palete

--- c# projects/OOPproject/Properties/Resources.Designer.cs
+++ c# projects/OOPproject/Properties/Resources.Designer.cs
@@ -103,6 +103,16 @@ namespace OOPproject.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap color_palette {
+            get {
+                object obj = ResourceManager.GetObject("color_palette", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap color_palette1 {
             get {
                 object obj = ResourceManager.GetObject("color-palette1", resourceCulture);
