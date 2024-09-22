f5af4aa7ec063a22c0259dbda6077c588aff3812
Yatal42 <taltal2345@gmail.com>
2024-09-23 02:38:08 +0300

    git ignore

--- OOPproject/obj/Debug/netcoreapp3.1/OOPproject.AssemblyInfo.cs
+++ OOPproject/obj/Debug/netcoreapp3.1/OOPproject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOPproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e36b64b3d54339635823e2085c1a9af50fe3139")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOPproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOPproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
